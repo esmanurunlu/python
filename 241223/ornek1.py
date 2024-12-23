0376f71428cfa4b80db882f12f29d72fed0fad78
esmanurunlu <esmanurunlu367@gmail.com>
2024-12-23 11:46:06 +0300

    Add files via upload

---
+++ 241223/ornek1.py
@@ -0,0 +1,4 @@
+birim_fiyat=20
+metre_kup=int(input("sayı giriniz"))
+su_fiyat=(birim_fiyat*metre_kup)
+print(su_fiyat,"kullandiniz suyun miktarı")
---
+++ 241223/ornek2.py
@@ -0,0 +1,6 @@
+not1=int(input("not1 i giriniz"))
+not2=int(input("not2 i giriniz"))
+perf1=int(input("perf1 i giriniz"))
+perf2=int(input("perf2 i giriniz")) 
+ort=(not1+not2+perf1+perf2)/4
+print("Ortalamanız :",ort)
