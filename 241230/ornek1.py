63f20f5c2290a7e7aece99512491801950d9da56
esmanurunlu <esmanurunlu367@gmail.com>
2024-12-30 11:32:21 +0300

    Add files via upload

---
+++ 241230/ornek1.py
@@ -0,0 +1,4 @@
+simit_adeti=int(input("simit adetini giriniz"))
+birim_fiyat=15
+toplam_tutar=birim_fiyat*simit_adeti
+print("simitlerin toplam fiyatı : ",toplam_tutar)
---
+++ 241230/ornek2.py
@@ -0,0 +1,5 @@
+hiz=int(input("hızı giriniz"))
+sure=int(input("süre girin "))
+mesafe=int(input("mesafe girin"))
+kalan_mesafe=(hiz*sure)-mesafe
+print("Kalan mesafeniz : ",kalan_mesafe)
